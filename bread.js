9bc17a57d0263eb9e8a5e21f0621bdc8ff9aede4
greyss1234 <livawrobel@gmail.com>
2025-11-17 19:29:05 -0800

    Add files via upload

---
+++ bread.js
@@ -0,0 +1,8 @@
+
+function servings01() {
+    document.getElementById("servings1").style.color = "red";
+}
+
+function servings02() {
+    document.getElementById("servings2").style.color = "blue";
+}
