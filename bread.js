a6873d588bbeea2dc01d69c6115a94eacd44e5bb
greyss1234 <livawrobel@gmail.com>
2025-11-24 00:49:21 -0800

    Add files via upload

--- bread.js
+++ bread.js
@@ -1,8 +1,28 @@
-
 function servings01() {
-    document.getElementById("servings1").style.color = "red";
+    document.getElementById("water1").innerHTML = "Lukewarm water (900mL)"
+    document.getElementById("salt").innerHTML = "Salt (1 1/2TBSP)"
+    document.getElementById("ryeflour").innerHTML = "Rye flour (500g)"
+    document.getElementById("whiteflour").innerHTML = "All-purpose flour (375g)"
+    document.getElementById("water2").innerHTML = "Lukewarm water (250mL)"
+    document.getElementById("ryekernels").innerHTML = "Rye kernels (500g)"
 }
 
 function servings02() {
-    document.getElementById("servings2").style.color = "blue";
+    document.getElementById("water1").innerHTML = "Lukewarm water (1750mL)"
+    document.getElementById("salt").innerHTML = "Salt (3TBSP)"
+    document.getElementById("ryeflour").innerHTML = "Rye flour (1000g)"
+    document.getElementById("whiteflour").innerHTML = "All-purpose flour (750g)"
+    document.getElementById("water2").innerHTML = "Lukewarm water (500mL)"
+    document.getElementById("ryekernels").innerHTML = "Rye kernels (1000g)"
+}
+
+function commenting() {
+    document.getElementById("hsy").innerHTML = "Your Username"
+  let x = document.getElementById("kdj").value;
+  document.getElementById("yjh").innerHTML = x;
+}
+
+function rating() {
+  let y = document.getElementById("jhw").value;
+  document.getElementById("lhs").innerHTML = "(Rating: " + y + ")";
 }
